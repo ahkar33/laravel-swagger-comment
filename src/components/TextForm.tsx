653be44e217605cb40b0ca15670dbfa1df9c4775
ahkar33 <ahkartoemaw@gmail.com>
2023-04-21 19:42:27 +0630

    added v2

--- src/components/TextForm.tsx
+++ src/components/TextForm.tsx
@@ -1,7 +1,7 @@
 import { useRef, useState } from "react";
 
 const TextForm = () => {
-	const [text, setText] = useState<string[]>([]);
+	const [formText, setFormText] = useState<string>("");
 	const textRef = useRef<HTMLTextAreaElement>(null);
 
 	const convertText = (e: React.SyntheticEvent) => {
@@ -11,25 +11,32 @@ const TextForm = () => {
 		}
 		let inputText = textRef.current.value
 			.replace(/\[/g, "{")
-			.replace(/\]/g, "}");
+			.replace(/\]/g, "}")
+			.trim();
+
 		let formattedTexts = inputText.split("\n");
 		formattedTexts = formattedTexts.map((eachLine: string) => {
 			return "*" + eachLine;
 		});
-		setText([...formattedTexts]);
+		const formattedTextsStr = formattedTexts.join("\n");
+		setFormText(formattedTextsStr);
+	};
+
+	const handleCopy = () => {
+		navigator.clipboard.writeText(formText);
 	};
 
 	return (
 		<div className="flex items-center flex-col mt-1">
 			<form onSubmit={convertText}>
 				<div className="flex flex-col items-center gap-2">
-					<label className="block text-sm font-medium text-gray-900 dark:text-white">
+					<label className="block text-xl font-bold text-blue-500">
 						Enter Your Api Response
 					</label>
 					<textarea
 						className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 						ref={textRef}
-						cols={90}
+						cols={150}
 						rows={15}
 					></textarea>
 					<div>
@@ -39,13 +46,21 @@ const TextForm = () => {
 					</div>
 				</div>
 			</form>
-			<div style={{ margin: "3rem" }}>
-				{text.map((text, index) => (
-					<pre key={index}>
-						<span>{text}</span>
-						<br />
-					</pre>
-				))}
+			<div className="mt-2">
+				<textarea
+					className="block p-2.5 w-full text-sm text-gray-900 bg-blue-100 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+					cols={150}
+					rows={15}
+					value={formText}
+				></textarea>
+				<div className="flex justify-center">
+					<button
+						className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded m-2"
+						onClick={handleCopy}
+					>
+						Copy Code
+					</button>
+				</div>
 			</div>
 		</div>
 	);
