89b5302298d85e58544cb4a08a0669faee1ece48
ahkar33 <ahkartoemaw@gmail.com>
2023-04-22 12:49:01 +0630

    fixed bugs

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,15 +3,13 @@ type ButtonProps = {
 	hoverBgColor?: string;
 	textColor?: string;
   text: string;
-  onClick: () => void
+  onClick?: () => void
 };
 
 const Button = ({ bgColor, hoverBgColor, textColor, text,onClick }: ButtonProps) => {
 	return (
 		<button
-			className={`bg-${bgColor ?? "blue-500"} hover:bg-${
-				hoverBgColor ?? "blue-700"
-			} text-${textColor ?? "white"} font-bold py-2 px-4 my-2 rounded`}
+			className={`bg-${bgColor ?? "blue-500"} hover:bg-${hoverBgColor ?? "blue-700"} text-${textColor ?? "white"} font-bold py-2 px-4 my-2 rounded`}
       onClick={onClick}
 		>
       {text}
--- src/components/TextForm.tsx
+++ src/components/TextForm.tsx
@@ -3,16 +3,12 @@ import Button from "./Button";
 
 const TextForm = () => {
 	const [formText, setFormText] = useState<string>("");
-	const textRef = useRef<HTMLTextAreaElement>(null);
+	const [rawText, setRawText] = useState<string>("");
 
-	const convertText = () => {
-		if (!textRef.current?.value) {
-			return;
-		}
-		let inputText = textRef.current.value
-			.replace(/\[/g, "{")
-			.replace(/\]/g, "}")
-			.trim();
+	const convertText = (e: React.SyntheticEvent) => {
+		e.preventDefault();
+		let inputText = rawText.replace(/\[/g, "{").replace(/\]/g, "}").trim();
+
 		let formattedTexts = inputText.split("\n");
 		formattedTexts = formattedTexts.map((eachLine: string) => {
 			return "*" + eachLine;
@@ -20,32 +16,37 @@ const TextForm = () => {
 		const formattedTextsStr = formattedTexts.join("\n");
 		setFormText(formattedTextsStr);
 	};
+
 	const handleCopy = () => {
 		navigator.clipboard.writeText(formText);
 	};
 
 	return (
 		<div className="flex items-center flex-col mt-1">
+			<form onSubmit={convertText}>
 				<div className="flex flex-col items-center gap-2">
 					<label className="block text-xl font-bold text-blue-500">
 						Enter Your Api Response
 					</label>
 					<textarea
-						className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-						ref={textRef}
+						className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
 						cols={150}
 						rows={15}
+						value={rawText}
+						onChange={(e) => setRawText(e.target.value)}
 					></textarea>
 					<div>
-						<Button onClick={convertText} text="Convert" />
+						<Button text="Convert" />
 					</div>
 				</div>
+			</form>
 			<div className="mt-2">
 				<textarea
-					className="block p-2.5 w-full text-sm text-gray-900 bg-blue-100 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+					className="block p-2.5 w-full text-sm text-gray-900 bg-blue-100 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
 					cols={150}
 					rows={15}
 					value={formText}
+					readOnly
 				></textarea>
 				<div className="flex justify-center">
 					<Button onClick={handleCopy} text="Copy Code" />
